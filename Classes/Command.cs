2f0fd5de47323985a874aade5ed85e87131d0395
ZLobbster <83872701+ZLobbster@users.noreply.github.com>
2021-06-04 13:34:18 +0700

    Update Command.cs

--- Classes/Command.cs
+++ Classes/Command.cs
@@ -1,17 +1,17 @@
-﻿using Telegram.Bot;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace Console_TelegramBot.Classes
 {
     public abstract class Command
     {
+        //абстрактный класс Command от которого будут наследовать свойства все остальные команды
+        
+        //имя команды
         public abstract string Name { get; }
 
+        //метод выполнения команды
         public abstract void Execute(Message message, TelegramBotClient Bot);
 
-        public bool Contains(string command)
-        {
-            return command.Contains(this.Name);
-        }
     }
 }
