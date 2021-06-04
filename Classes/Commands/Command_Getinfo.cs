611caa706838af1b85d866d726e7882b125ba574
ZLobbster <83872701+ZLobbster@users.noreply.github.com>
2021-06-04 13:35:20 +0700

    Update Command_Getinfo.cs

--- Classes/Commands/Command_Getinfo.cs
+++ Classes/Commands/Command_Getinfo.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace Console_TelegramBot.Classes.Commands
 {
+    //наследование от абстрактного класса Command
     class Command_Getinfo : Command
     {
         public override string Name => "/getinfo";
@@ -12,6 +13,8 @@ namespace Console_TelegramBot.Classes.Commands
         public override async void Execute(Message message, TelegramBotClient Bot)
         {
             Thread.Sleep(500);
+
+            //создание кнопок под сообщением
             var button_1 = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardButton();
             var button_2 = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardButton();
             var button_3 = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardButton();
@@ -37,6 +40,7 @@ namespace Console_TelegramBot.Classes.Commands
             var keyboard = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup(buttons);
 
             Console.WriteLine("    #Sending info...\n");
+            //отправка пользователю сообщения с кнопками, содержащими в себе ссылки
             await Bot.SendTextMessageAsync(message.Chat.Id, "This bot was created by Zlobbster using the Spoonacular API and C# Telegram Bot API",
                 Telegram.Bot.Types.Enums.ParseMode.Default, false, false, 0, keyboard);
         }
