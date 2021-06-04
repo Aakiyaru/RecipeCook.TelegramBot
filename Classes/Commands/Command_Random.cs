6adc26268fc780074405f6a0ed8998275c08b76a
ZLobbster <83872701+ZLobbster@users.noreply.github.com>
2021-06-04 13:35:38 +0700

    Update Command_Random.cs

--- Classes/Commands/Command_Random.cs
+++ Classes/Commands/Command_Random.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Net;
 using System.IO;
-using Telegram.Bot.Args;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 
 namespace Console_TelegramBot.Classes.Commands
 {
+    //наследование от абстрактного класса Command
     class Command_Random : Command
     {
         public override string Name => "/random";
 
         public override async void Execute(Message message, TelegramBotClient Bot)
         {
+            //создание случайного числа, которое будет id для запроса
             Random random = new Random();
 
             int recipe_id = random.Next(1, 999999);
 
             Console.WriteLine("    #Searching for recipe...");
 
+            //создаём запрос на Spoonacular API с использованием полученного id
             HttpWebRequest recipe_request = (HttpWebRequest)WebRequest.Create("https://api.spoonacular.com/recipes/" + recipe_id + "/information?apiKey=eaaa3a4d5e9046d0800682645d0a4cd2");
             HttpWebResponse recipe_response = (HttpWebResponse)recipe_request.GetResponse();
 
@@ -36,6 +34,7 @@ namespace Console_TelegramBot.Classes.Commands
 
             dynamic recipe_d = JsonConvert.DeserializeObject(recipe_SReadData);
 
+            //считываем данные из полученного JSON пакета и распеределяем по переменным
             bool vegetarian = recipe_d.vegetarian;
             Console.WriteLine("        #Vegetarian: " + vegetarian);
 
@@ -57,10 +56,13 @@ namespace Console_TelegramBot.Classes.Commands
             int healthScore = recipe_d.healthScore;
             Console.WriteLine("        #healthScore: " + healthScore);
 
+            //создаём переменную первого сообщения в котором будут баллы блюда
             string recipe_Scores = "";
 
+            //отправка пользователю фотографии блюда
             await Bot.SendPhotoAsync(message.Chat.Id, recipe_d.image.ToString());
 
+            //редактирование первого сообщения
             recipe_Scores += (char.ConvertFromUtf32(0x1F44D) + " Aggregate Likes: " + aggregateLikes + "\n");
             recipe_Scores += (char.ConvertFromUtf32(0x2764) + " Health Score: " + healthScore + "\n");
             recipe_Scores += (char.ConvertFromUtf32(0x1F354) + " Weight Watcher Smart Points: " + weightWatcherSmartPoints + "\n");
@@ -101,10 +103,14 @@ namespace Console_TelegramBot.Classes.Commands
                 recipe_Scores += (char.ConvertFromUtf32(0x1F34F) + " Not suitable for vegetarians and vegans\n");
             }
 
+            //отправка первого сообщения
             await Bot.SendTextMessageAsync(message.Chat.Id, recipe_Scores);
 
             Console.WriteLine("        #Getting recipe...");
+            //создание второго сообщения - самого рецепта
             string instructions = recipe_d.instructions;
+
+            //удаление из него тегов HTML
             Regex ol = new Regex(@"<ol>");
             Regex ol2 = new Regex(@"</ol>");
             Regex li = new Regex(@"<li>");
@@ -118,6 +124,7 @@ namespace Console_TelegramBot.Classes.Commands
 
             Console.WriteLine("        #Recipe sended");
             Console.WriteLine("        #Task complete\n");
+            //отправка второго сообщения
             await Bot.SendTextMessageAsync(message.Chat.Id, instructions);
 
             Console.WriteLine("");
