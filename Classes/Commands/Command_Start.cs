a91a9cfe70ef73ec4e7c592d7fc1f4649b1eb6dd
ZLobbster <83872701+ZLobbster@users.noreply.github.com>
2021-06-04 13:36:20 +0700

    Update Command_Start.cs

--- Classes/Commands/Command_Start.cs
+++ Classes/Commands/Command_Start.cs
@@ -1,4 +1,4 @@
-﻿using Telegram.Bot;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 using System.Threading;
 
@@ -6,10 +6,13 @@ namespace Console_TelegramBot.Classes.Commands
 {
     class Command_Start : Command
     {
+        //наследование от абстрактного класса Command
         public override string Name => "/start";
 
         public override async void Execute(Message message, TelegramBotClient Bot)
         {
+            //отправляем пользователю фото и текст
+
             await Bot.SendPhotoAsync(message.Chat.Id,
                 "https://culinaryschool.ru/wp-content/uploads/2016/07/10.-%D0%A1%D1%83-%D1%88%D0%B5%D1%84-%D0%BF%D0%BE%D0%B2%D0%B0%D1%80.jpg",
                 "Hi there! I am Recipe Cook Bot and I will help you with your cooking! I know many different recipes.");
